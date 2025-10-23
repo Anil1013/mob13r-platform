8be035fa8b5f4e6166b22a0a46711eec83e0cf68
Anil Kumar <ruhil.13r@gmail.com>
2025-10-23 12:50:26 +0530

    Add /api root route for AWS health check

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 const router = express.Router();
 
+// Default API root route
 router.get("/", (req, res) => {
-  res.json({ message: "Mob13r Backend API running successfully!" });
+  res.json({
+    status: "API working 🚀",
+    message: "Welcome to Mob13r backend",
+  });
 });
 
+// Example: add DB check or health route later
 export default router;
+
