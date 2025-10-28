ac0564a7c2761f0cacd518c28235e3c77fd27766
Anil1013 <ruhil.13r@gmail.com>
2025-10-28 22:51:31 +0530

    Delete backend directory

--- backend/.platform/hooks/postdeploy/01_seed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🌱 Running DB seed automatically on deploy..."
-
-# Run from current directory (no need to cd)
-node seed.js || echo "⚠️ Seed failed — check logs"
-
--- backend/.platform/hooks/prebuild/01_node_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "⚙️ Ensuring Node.js 20.x is installed..."
-sudo dnf install -y nodejs-20 || sudo dnf install -y nodejs
