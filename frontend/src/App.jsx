6993aeca4c0a177e9f8c77bb8ced51c3dd338a4f
Anil1013 <ruhil.13r@gmail.com>
2025-10-29 03:32:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import Dashboard from "./pages/Dashboard";
-import Advertisers from "./pages/Advertisers";
-import Publishers from "./pages/Publishers";
-import Clicks from "./pages/Clicks";
-import Conversions from "./pages/Conversions";
-import Postbacks from "./pages/Postbacks";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Sidebar from "./components/Sidebar.jsx";
+import Header from "./components/Header.jsx";
+
+// Pages
+import Dashboard from "./pages/Dashboard.jsx";
+import Advertisers from "./pages/Advertisers.jsx";
+import Publishers from "./pages/Publishers.jsx";
+import Clicks from "./pages/Clicks.jsx";
+import Conversions from "./pages/Conversions.jsx";
+import Postbacks from "./pages/Postbacks.jsx";
 
 function App() {
   return (
     <Router>
-      <div className="flex min-h-screen bg-gray-100">
+      <div className="flex min-h-screen bg-gray-50 text-gray-900">
+        {/* Sidebar */}
         <Sidebar />
+
+        {/* Main Content */}
         <div className="flex-1 flex flex-col">
           <Header />
-          <main className="p-6 flex-1 overflow-y-auto">
+
+          <main className="flex-1 overflow-y-auto p-6">
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/advertisers" element={<Advertisers />} />
@@ -24,6 +30,9 @@ function App() {
               <Route path="/clicks" element={<Clicks />} />
               <Route path="/conversions" element={<Conversions />} />
               <Route path="/postbacks" element={<Postbacks />} />
+
+              {/* Redirect unknown routes to Dashboard */}
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
         </div>
