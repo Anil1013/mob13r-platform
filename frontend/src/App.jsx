08a0f50c8ba955d3cdc04d21366310be2b823c91
Anil1013 <ruhil.13r@gmail.com>
2025-10-29 11:11:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar.jsx";
 import Header from "./components/Header.jsx";
 
@@ -13,31 +13,29 @@ import Postbacks from "./pages/Postbacks.jsx";
 
 function App() {
   return (
-    <Router>
-      <div className="flex min-h-screen bg-gray-50 text-gray-900">
-        {/* Sidebar */}
-        <Sidebar />
+    <div className="flex min-h-screen bg-gray-50 text-gray-900">
+      {/* Sidebar (Navigation Menu) */}
+      <Sidebar />
 
-        {/* Main Content */}
-        <div className="flex-1 flex flex-col">
-          <Header />
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col">
+        <Header />
 
-          <main className="flex-1 overflow-y-auto p-6">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/advertisers" element={<Advertisers />} />
-              <Route path="/publishers" element={<Publishers />} />
-              <Route path="/clicks" element={<Clicks />} />
-              <Route path="/conversions" element={<Conversions />} />
-              <Route path="/postbacks" element={<Postbacks />} />
+        <main className="flex-1 overflow-y-auto p-6">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/advertisers" element={<Advertisers />} />
+            <Route path="/publishers" element={<Publishers />} />
+            <Route path="/clicks" element={<Clicks />} />
+            <Route path="/conversions" element={<Conversions />} />
+            <Route path="/postbacks" element={<Postbacks />} />
 
-              {/* Redirect unknown routes to Dashboard */}
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </main>
-        </div>
+            {/* Redirect any invalid routes to dashboard */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
       </div>
-    </Router>
+    </div>
   );
 }
 
