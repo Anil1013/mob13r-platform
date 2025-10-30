1dd3977f574ca024f9c4a529afd8416888b53dd4
Anil1013 <ruhil.13r@gmail.com>
2025-10-30 14:59:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Sidebar from "./components/Sidebar.jsx";
 import Header from "./components/Header.jsx";
 
@@ -10,10 +11,11 @@ import Publishers from "./pages/Publishers.jsx";
 import Clicks from "./pages/Clicks.jsx";
 import Conversions from "./pages/Conversions.jsx";
 import Postbacks from "./pages/Postbacks.jsx";
+import Offers from "./pages/Offers.jsx"; // ✅ Make sure this file exists
 
 function App() {
   return (
-    <div className="flex min-h-screen bg-gray-50 text-gray-900">
+    <div className="flex min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
@@ -25,6 +27,7 @@ function App() {
             <Route path="/clicks" element={<Clicks />} />
             <Route path="/conversions" element={<Conversions />} />
             <Route path="/postbacks" element={<Postbacks />} />
+            <Route path="/offers" element={<Offers />} /> {/* ✅ new route */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
