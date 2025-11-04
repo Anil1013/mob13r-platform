8cd7730429df235ecf01fd1530d675bb4e01f53b
Anil1013 <ruhil.13r@gmail.com>
2025-11-04 13:09:06 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,4 +12,12 @@ app.use(cors({
   allowedHeaders: "Content-Type, Authorization"
 }));
 
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://dashboard.mob13r.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
+
 app.use(express.json());
