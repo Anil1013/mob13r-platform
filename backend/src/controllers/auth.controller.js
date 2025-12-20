347784f27af654b6847b6536e9463228c4158170
Anil1013 <ruhil.13r@gmail.com>
2025-12-21 03:24:18 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,15 +4,14 @@ import bcrypt from "bcrypt";
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
-  // TEMP ADMIN LOGIN (later DB se aayega)
   if (email !== "admin@mob13r.com") {
     return res.status(401).json({ message: "Invalid credentials" });
   }
 
-  const valid = await bcrypt.compare(
-    password,
-    "$2b$10$svgCr6DiTJu/a8wZC2ZWLhuC6sA6IK6Hu8ALykd8KfjcECYEq7Fa" // Admin@123 hash
-  );
+  const ADMIN_HASH =
+    "$2b$10$vgCr6DiTJu/a8wZC2ZWLhuC6sA6IK6Hu8ALykdk8KfjcECYQa7Fa";
+
+  const valid = await bcrypt.compare(password, ADMIN_HASH);
 
   if (!valid) {
     return res.status(401).json({ message: "Invalid credentials" });
@@ -29,4 +28,3 @@ export const login = async (req, res) => {
     user: { email, role: "admin" },
   });
 };
-
