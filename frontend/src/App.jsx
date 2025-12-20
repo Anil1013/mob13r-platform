c5a8036ede2bb2e4d49c79719b4a96c0aaa88222
Anil1013 <ruhil.13r@gmail.com>
2025-12-20 21:22:36 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,27 +4,18 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Advertisers from "./pages/Advertisers";
 import Offers from "./pages/Offers";
-import OfferForm from "./pages/OfferForm";
-import OfferConfig from "./pages/OfferConfig";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Auth */}
         <Route path="/login" element={<Login />} />
 
-        {/* Main Pages */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/advertisers" element={<Advertisers />} />
         <Route path="/offers" element={<Offers />} />
 
-        {/* Internal Offer Routes (NOT in sidebar) */}
-        <Route path="/offers/new" element={<OfferForm />} />
-        <Route path="/offers/:id/edit" element={<OfferForm />} />
-        <Route path="/offers/:id/config" element={<OfferConfig />} />
-
-        {/* Fallback */}
+        {/* fallback */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
