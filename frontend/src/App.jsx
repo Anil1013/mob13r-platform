8291abe12fdf0c39721929926d6ac7ee24f511b2
Anil1013 <ruhil.13r@gmail.com>
2025-12-20 17:23:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Advertisers from "./pages/Advertisers";
+
 
 export default function App() {
   return (
@@ -8,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/advertisers" element={<Advertisers />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
