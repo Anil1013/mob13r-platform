745490270386186db4b9b01d729cd71e7368b53c
Anil1013 <ruhil.13r@gmail.com>
2025-12-21 18:59:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,33 @@ import Dashboard from "./pages/Dashboard";
 import Advertisers from "./pages/Advertisers";
 import Offers from "./pages/Offers";
 
+import ProtectedRoute from "./auth/ProtectedRoute";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* 🔓 Public */}
         <Route path="/login" element={<Login />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/advertisers" element={<Advertisers />} />
-        <Route path="/offers" element={<Offers />} />
+        {/* 🔐 ALL PROTECTED ROUTES */}
+        <Route
+          element={
+            <ProtectedRoute>
+              {/* 👇 all protected pages live here */}
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/advertisers" element={<Advertisers />} />
+                <Route path="/offers" element={<Offers />} />
+              </Routes>
+            </ProtectedRoute>
+          }
+        />
 
         {/* fallback */}
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+
       </Routes>
     </BrowserRouter>
   );
