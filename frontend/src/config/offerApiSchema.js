0f47d1f5876ec699f96fad71235c337117d0a991
Anil1013 <ruhil.13r@gmail.com>
2025-12-25 23:07:20 +0530

    Create offerApiSchema.js

---
+++ frontend/src/config/offerApiSchema.js
@@ -0,0 +1,91 @@
+export const OFFER_API_SCHEMA = {
+  status_check: {
+    label: "Status Check",
+    description: "Check subscription / eligibility status",
+    default: {
+      enabled: true,
+      method: "GET",
+      url: "",
+      headers: {},
+      params: {},
+      success_matcher: "",
+    },
+    templates: [
+      "msisdn",
+      "transaction_id",
+      "ip",
+      "user_ip",
+      "ua",
+      "param1",
+      "param2",
+    ],
+  },
+
+  pin_send: {
+    label: "PIN Send",
+    description: "Generate OTP / PIN",
+    default: {
+      enabled: true,
+      method: "POST",
+      url: "",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      params: {},
+      success_matcher: "",
+    },
+    templates: [
+      "msisdn",
+      "transaction_id",
+      "uuid_trxid",
+      "user_ip",
+      "ua",
+      "param1",
+      "param2",
+    ],
+  },
+
+  pin_verify: {
+    label: "PIN Verify",
+    description: "Verify OTP / PIN",
+    default: {
+      enabled: true,
+      method: "POST",
+      url: "",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      params: {},
+      success_matcher: "",
+    },
+    templates: [
+      "msisdn",
+      "transaction_id",
+      "pin",
+      "user_ip",
+      "ua",
+      "param1",
+      "param2",
+    ],
+  },
+
+  anti_fraud: {
+    label: "Anti-Fraud",
+    description: "Optional fraud detection step",
+    default: {
+      enabled: false,
+      method: "POST",
+      url: "",
+      headers: {},
+      params: {},
+      success_matcher: "",
+    },
+    templates: [
+      "msisdn",
+      "transaction_id",
+      "anti_fraud_id",
+      "ip",
+      "ua",
+    ],
+  },
+};
