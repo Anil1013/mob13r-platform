06703e2ea5d2ee113b43b53b690a05d302907c41
Anil1013 <ruhil.13r@gmail.com>
2025-12-27 18:22:45 +0530

    Create db.js

---
+++ backend/src/db.js
@@ -0,0 +1,15 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 5432,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
+});
+
+export default pool;
