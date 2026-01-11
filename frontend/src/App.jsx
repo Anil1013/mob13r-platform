faa538ce6d918e0f4e0368f5f09773ec8082e678
Anil1013 <ruhil.13r@gmail.com>
2026-01-11 17:31:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Offers from "./pages/Offers";
 import Publishers from "./pages/Publishers";
 import PublisherDashboard from "./pages/PublisherDashboard";
 import PublisherAssignOffers from "./pages/PublisherAssignOffers.jsx";
+import DumpDashboard from "./pages/DumpDashboard";
 import ProtectedRoute from "./auth/ProtectedRoute";
 
 export default function App() {
@@ -23,6 +24,7 @@ export default function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/publishers" element={<Publishers />} />
           <Route path="/publishers/assign" element={<PublisherAssignOffers />} />
+          <Route path="/dashboard/dump" element={<DumpDashboard />} />
           <Route path="/publisher/dashboard" element={<PublisherDashboard />} />
         </Route>
 
