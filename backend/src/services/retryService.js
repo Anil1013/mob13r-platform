622455ff2e2d3453053dc33702f9b974403e9c9a
Anil1013 <ruhil.13r@gmail.com>
2026-02-15 21:28:18 +0530

    Delete backend/src/services/retryService.js

--- backend/src/services/retryService.js
+++
@@ -1,8 +0,0 @@
-export async function retryCall(fn, retries = 2) {
-  try {
-    return await fn();
-  } catch (err) {
-    if (retries === 0) throw err;
-    return retryCall(fn, retries - 1);
-  }
-}
