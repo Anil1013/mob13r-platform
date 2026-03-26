7d400f88167671912e243b27a6ed6983bda1ad11
Anil1013 <ruhil.13r@gmail.com>
2026-03-26 15:59:17 +0530

    Update PublisherDashboard.jsx

--- frontend/src/pages/PublisherDashboard.jsx
+++ frontend/src/pages/PublisherDashboard.jsx
@@ -17,11 +17,19 @@ const formatDateOnly = (value) => {
 
 const formatDateTime = (value) => {
   if (!value) return "-";
-  let v = value.replace("T", " ").replace("Z", "").split(".")[0];
-  const [date, time] = v.split(" ");
-  if (!date || !time) return "-";
-  const [y, m, d] = date.split("-");
-  return `${d}/${m}/${y}, ${time}`;
+
+  const d = new Date(value);
+
+  return d.toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+  });
 };
 
 const hourLabel = (hourValue) => {
