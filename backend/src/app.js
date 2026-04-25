d6f10cf5519ec735421764a97abe7f920dca3ed5
Anil1013 <ruhil.13r@gmail.com>
2026-04-25 23:09:26 +0530

    Add file upload middleware to the app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import fileUpload from "express-fileupload";
 
 /* -------- ROUTES -------- */
 import authRoutes from "./routes/auth.routes.js";
@@ -26,6 +27,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(fileUpload());
 app.use('/uploads', express.static('public/uploads'));
 
 /* -------- HEALTH CHECK -------- */
